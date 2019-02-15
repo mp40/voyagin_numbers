4caae131b0468a1a89abe6ad78ebad20b32b869f
Matthew Philps <philps.matthew@gmail.com>
2019-02-15 12:18:41 +0900

    refactored

--- arabic2english.js
+++ arabic2english.js
@@ -2,9 +2,16 @@ function arabic2english (num) {
   if (num < 20) {
     return one2nineteen(num)
   }
-  const decadeWords = ['twenty', 'thirty']
-  let decade = decadeWords[Math.floor(num / 10) - 2]
-  return num - Math.floor(num / 10) * 10 > 0 ? `${decade} ${one2nineteen(num - 20)}` : `${decade}`
+  const decade = Math.floor(num / 10) * 10
+  return num - decade > 0 ? `${twentyUpwards(decade)} ${one2nineteen(num - decade)}` : `${twentyUpwards(decade)}`
+}
+
+function twentyUpwards (num) {
+  const decadeWords = [
+    'twenty',
+    'thirty'
+  ]
+  return decadeWords[num / 10 - 2]
 }
 
 function one2nineteen (num) {
