34cb8a2e3928e3f4255a37256152a55e468639df
Matthew Philps <philps.matthew@gmail.com>
2019-02-15 13:46:46 +0900

    passed test for 200

--- arabic2english.js
+++ arabic2english.js
@@ -6,14 +6,17 @@ function arabic2english (num) {
     const decade = Math.floor(num / 10) * 10
     return num - decade > 0 ? `${twenty2ninety(decade)} ${one2nineteen(num - decade)}` : `${twenty2ninety(decade)}`
   }
-  if (num === 100) {
-    return 'one hundred'
-  } else {
-    let temp = arabic2english(num - 100)
-    return 'one hundred and ' + temp
+  if (num < 1000) {
+    const hundreds = Math.floor(num / 100)
+    let str = `${one2nineteen(hundreds)} hundred`
+    if (num === hundreds * 100) {
+      return str
+    } else {
+      let temp = arabic2english(num - 100)
+      return `${str} and ${temp}`
+    }
   }
 }
-
 function twenty2ninety (num) {
   const decadeWords = [
     'twenty',
